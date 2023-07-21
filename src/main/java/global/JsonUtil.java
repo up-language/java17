4458258aa79d05f987cf1ae46115227018dde351
JavaCommons Technologies <javacommons@gmail.com>
2023-07-21 18:46:52 +0900

    .

---
+++ src/main/java/global/JsonUtil.java
@@ -0,0 +1,30 @@
+package global;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import jakarta.json.*;
+
+public class JsonUtil {
+
+    static public String stringify(JsonStructure jsonObjectOrArray) {
+        StringWriter stWriter = new StringWriter();
+        JsonWriter jsonWriter = Json.createWriter(stWriter);
+        jsonWriter.write(jsonObjectOrArray);
+        jsonWriter.close();
+        String jsonString = stWriter.toString();
+        return jsonString;
+    }
+
+    static public JsonObject parse(String jsonObjectString) {
+        JsonReader jsonReader = Json.createReader(new StringReader(jsonObjectString));
+        JsonObject jsonObject = jsonReader.readObject();
+        return jsonObject;
+    }
+
+    static public JsonArray parseArray(String jsonArrayString) {
+        JsonReader jsonReader = Json.createReader(new StringReader(jsonArrayString));
+        JsonArray jsonArray = jsonReader.readArray();
+        return jsonArray;
+    }
+}
