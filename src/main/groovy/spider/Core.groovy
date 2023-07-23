a6fbcc0b2b33145872bf5e3b3a76e9f610693625
JavaCommons Technologies <javacommons@gmail.com>
2023-07-23 15:18:33 +0900

    .

--- src/main/groovy/spider/Core.groovy
+++ src/main/groovy/spider/Core.groovy
@@ -6,31 +6,30 @@ import system.MiscUtil
 
 class Core {
     def vm = null
-    def info = [:]
+    def info = null
     Core(GroovyVM vm) {
         this.vm = vm
+        this.info = readSettings("spider-explorer")
     }
     def info() {
-        info = [:]
-        String userHomeDir = System.getProperty("user.home")
-        info.userHomeDir = userHomeDir
-        System.out.printf("The User Home Directory is %s\n", userHomeDir)
-        String settingsPath = userHomeDir + "/.apps/spider-explorer/settings.json";
-        info.settingsPath = settingsPath
-        String json = vm.readStringFromFile(settingsPath, '{ "count": 0 }')
-        def obj = vm.fromJson(json)
-        vm.echo(obj);
-        vm.echo(obj.xyz, "obj.xyz")
-        obj.xyz = "xyz"
-        obj.count = obj.count + 1
-        info.obj = obj
-        json = vm.toJson(obj)
-        vm.echo(json)
-        vm.writeStringToFile(settingsPath, json)
+        if (this.info.info.count == null) this.info.info.count = 0
+        this.info.info.count++
         return info
     }
     def stripTest(inf) {
         inf = Dynamic.strip(inf)
         vm.echo(inf)
+    }
+    def readSettings(product) {
+        def info = [:]
+        String userHomeDir = System.getProperty("user.home")
+        info.userHomeDir = userHomeDir
+        println "The User Home Directory is $userHomeDir"
+        String settingsPath = userHomeDir + "/$product/settings.json";
+        info.path = settingsPath
+        String json = vm.readStringFromFile(settingsPath, '{}')
+        def obj = vm.fromJson(json)
+        info.info = obj
+        return info
     }
 }
