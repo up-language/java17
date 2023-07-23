25768fc981b152c5151c1040f231ed36ef5c7613
JavaCommons Technologies <javacommons@gmail.com>
2023-07-23 15:24:10 +0900

    .

--- src/main/groovy/spider/Core.groovy
+++ src/main/groovy/spider/Core.groovy
@@ -2,14 +2,13 @@ package spider
 
 import system.Dynamic
 import system.GroovyVM
-import system.MiscUtil
 
 class Core {
     def vm = null
     def info = null
     Core(GroovyVM vm) {
         this.vm = vm
-        this.info = readSettings("spider-explorer")
+        this.info = loadSettings("spider-explorer")
     }
     def info() {
         if (this.info.info.count == null) this.info.info.count = 0
@@ -20,7 +19,7 @@ class Core {
         inf = Dynamic.strip(inf)
         vm.echo(inf)
     }
-    def readSettings(product) {
+    def loadSettings(product) {
         def info = [:]
         String userHomeDir = System.getProperty("user.home")
         info.userHomeDir = userHomeDir
@@ -31,5 +30,9 @@ class Core {
         def obj = vm.fromJson(json)
         info.info = obj
         return info
+    }
+    def saveSettings(settings) {
+        def path = settings.path
+        vm.writeStringToFile(path, vm.toJson(settings.info))
     }
 }
